b7b007e2d9562be08eed1010ea795957a2c7abce
Kevin Wellenzohn <kevin@blockshake.io>
2023-10-19 08:24:56 +0200

    Drop unused SQL files

--- db/queries/pending_logins_create.sql
+++
@@ -1,9 +0,0 @@
-insert into pending_logins(session_id, username, session_key, expected_client_mac, created_at, expires_at)
-values (
-    $1,
-    $2,
-    $3,
-    $4,
-    current_timestamp,
-    current_timestamp + ($5 || ' minutes')::interval
-);
--- db/queries/pending_registration_drop.sql
+++
@@ -1,5 +0,0 @@
-delete from pending_registrations
-where
-    session_id = $1 and
-    current_timestamp < expires_at
-returning *;
